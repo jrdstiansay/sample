87d752e05350fa01e7c249f56566b2fa5de2d01d
jrdstiansay <63148101+jrdstiansay@users.noreply.github.com>
2022-02-06 21:30:24 +0800

    Init commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const path = require("path");
+const express = require("express");
+const { engine } = require("express-handlebars");
+
+const app = express();
+
+const publicDirectoryPath = path.join(__dirname, "../public");
+
+app.engine("handlebars", engine());
+
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+
+app.use(express.static(publicDirectoryPath));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(5000);
